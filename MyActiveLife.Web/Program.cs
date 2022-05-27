01062c16b184443980c92ccbd87d3183f49203eb
Brent Maxwell <brent@thebrent.net>
2022-05-27 11:52:20 -0400

    added Strava login to web project

--- MyActiveLife.Web/Program.cs
+++ MyActiveLife.Web/Program.cs
@@ -14,6 +14,14 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
+builder.Services.AddAuthentication()
+    .AddStrava(options =>
+    {
+        IConfigurationSection stravaOAuthConfigSection = builder.Configuration.GetSection("Authentication:Strava");
+        options.ClientId = stravaOAuthConfigSection["ClientId"];
+        options.ClientSecret = stravaOAuthConfigSection["ClientSecret"];
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
