b086d5c5faae480ae370fc9a2237814b7029cfa2
Brent Maxwell <brent@thebrent.net>
2022-06-07 13:05:00 -0400

    Weather skeleton

---
+++ MyActiveLife.Apis.Weather/Entities/DailyWeather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyActiveLife.Apis.Weather.Entities
+{
+    public class DailyWeather
+    {
+        public DateTime Date { get; set; }
+    }
+}
---
+++ MyActiveLife.Apis.Weather/WeatherClient.cs
@@ -0,0 +1,27 @@
+﻿using MyActiveLife.Apis.Weather.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyActiveLife.Apis.Weather
+{
+    public class WeatherClient
+    {
+        public WeatherClient()
+        {
+
+        }
+
+        public DailyWeather GetWeather(DateTime date)
+        {
+            return new DailyWeather();
+        }
+
+        public ICollection<DailyWeather> GetWeather(DateTime startDate, DateTime endDate)
+        {
+            return new List<DailyWeather>();
+        }
+    }
+}
