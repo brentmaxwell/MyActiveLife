b33642c62ee0a56c61eaa34e92064d97f81024e9
Brent Maxwell <brent@thebrent.net>
2022-06-22 12:19:27 -0400

    added withings base

---
+++ MyActiveLife.Apis.Withings/Entities/Measure.cs
@@ -0,0 +1,22 @@
+﻿using MyActiveLife.Apis.Withings.Enums;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyActiveLife.Apis.Withings.Entities
+{
+    public class Measure
+    {
+        [JsonProperty("value")]
+        public int Value { get; set; }
+
+        [JsonProperty("unit")]
+        public int Unit { get; set; }
+
+        [JsonProperty("type")]
+        public MeasureTypeEnum Type { get; set; }
+    }
+}
---
+++ MyActiveLife.Apis.Withings/Enums/MeasureTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyActiveLife.Apis.Withings.Enums
+{
+    public enum MeasureTypeEnum
+    {
+        Weight = 1,
+        Height = 4
+    }
+}
